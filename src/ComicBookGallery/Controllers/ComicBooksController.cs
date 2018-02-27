cfbeaa04e6611e267eb7729bf31db4b65ec16307
gminovska <ekogoca@gmail.com>
2018-02-27 15:52:39 -0800

    Update Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,9 +9,21 @@ namespace ComicBookGallery.Controllers
     //Controller classes and the action methods have to be public
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        //The action methods return Action Result types (base class ActionResult), such as ContentResult or RedirectResult
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller!";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+                //return new RedirectResult("/");
+            }
+
+            return Content("This is a method in the Controller base class that simplifies our code");
+        //    return new ContentResult() {
+        //        //Pressing Alt + F12 gives us a pek into the class definition 
+        //        Content = "Hello from the comic books controller!"
+        //};
+            
         }
     }
 }
