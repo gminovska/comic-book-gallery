684e62b617e7e754f6f85506d5c3bdffc34ac0df
gminovska <ekogoca@gmail.com>
2018-02-27 15:20:56 -0800

    Add Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,8 +6,12 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
+    //Controller classes and the action methods have to be public
     public class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
