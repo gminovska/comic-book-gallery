80869dbdd00f47746bcdfcb5e52d0d5d4208152a
gminovska <ekogoca@gmail.com>
2018-02-27 17:43:15 -0800

    Add Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,17 +12,18 @@ namespace ComicBookGallery.Controllers
         //The action methods return Action Result types (base class ActionResult), such as ContentResult or RedirectResult
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
+            return View();
+        //    if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+        //    {
+        //        return Redirect("/");
+        //        //return new RedirectResult("/");
+        //    }
 
-            return Content("This is a method in the Controller base class that simplifies our code");
-        //    return new ContentResult() {
-        //        //Pressing Alt + F12 gives us a pek into the class definition 
-        //        Content = "Hello from the comic books controller!"
-        //};
+        //    return Content("This is a method in the Controller base class that simplifies our code");
+        ////    return new ContentResult() {
+        ////        //Pressing Alt + F12 gives us a pek into the class definition 
+        ////        Content = "Hello from the comic books controller!"
+        ////};
             
         }
     }
