0991149c2ebadb30c6ae00bfed0a4425c610b1f7
gminovska <ekogoca@gmail.com>
2018-03-01 16:07:32 -0800

    Add comic book repository class

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,23 +13,8 @@ namespace ComicBookGallery.Controllers
         //The action methods return Action Result types (base class ActionResult), such as ContentResult or RedirectResult
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
-
-                }
-            }; 
-
-            return View(comicBook);
+            
+            return View();
         }
     }
 }
